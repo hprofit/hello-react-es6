4a4c2936a7b1fd1f9a815baa5a3d9476be6b4b14
holden_profit <holdenprofit@gmail.com>
2016-07-16 10:10:38 -0400

    Updating userdetails.

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,12 +1,6 @@
 import React from 'react'
 
 class UserDetails extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {};
-  //   this.render = this.render.bind(this);
-  // }
-
   render() {
     var id = this.props.params.id;
     var userDetail = userList[id] || {name:'',email:''};
